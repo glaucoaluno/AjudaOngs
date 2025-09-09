814bfbfd8b3ad948740b5afc588d2cfeae340bd1
glaucoaluno <glauco.neto@aluno.ifsp.edu.br>
2025-09-09 19:57:22 -0300

    Alterados arquivos docker.

--- scripts/run-tests-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Executando testes dentro do container Docker..."
-
-# Executar testes dentro do container
-docker compose exec app php artisan test
-
-echo "Testes concluídos!" 
