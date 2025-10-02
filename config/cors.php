13216ffa8187e1c949b8e8770842c4b24fb8d9b7
glaucoaluno <glauco.neto@aluno.ifsp.edu.br>
2025-10-02 11:55:56 -0300

    Alteradas propriedades do CORS

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['/api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => ['https://sistema-doacoes.up.railway.app'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://sistema-doacoes.up.railway.app',],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => [''],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'support_credentials' => false
+    'supports_credentials' => false,
 ];
