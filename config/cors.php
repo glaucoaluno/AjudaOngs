39ccac44990360efd169a872b01e48dee80c5b5d
glaucoaluno <glauco.neto@aluno.ifsp.edu.br>
2025-10-02 11:36:54 -0300

    Alterado cors do laravel

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'paths' => ['/api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'paths' => ['/api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    'allowed_origins' => ['https://tcfinal-development.up.railway.app'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [''],
     'exposed_headers' => [],
     'max_age' => 0,
     'support_credentials' => false
